54e0f3b1745c6ad6607ec97baecfd5f69943a012
Somenath Choudhury <somenathchoudhury35@gmail.com>
2023-11-28 12:18:17 +0530

    commited

---
+++ README.md
@@ -0,0 +1,1 @@
+gradio app.py --demo-name myapp
--- app.py
+++ app.py
@@ -1,41 +1,43 @@
+import gradio as gr
 from transformers import pipeline
-from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+import re
 
 
-app = FastAPI()
 
-origins = ["*"]
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+text_summarizer_pipeline = pipeline('summarization')
 
 
-summarizer = pipeline('summarization')
+
+def greetMe(articleInput, min_length_of_article, max_length_of_article):
+
+    articleToBeSummarized = articleInput
+
+    if len(re.findall(r'\w+', articleToBeSummarized)) < 200:
+
+        return "The minimum length of the total article should be of minimum 200 words."
+
+    elif len(re.findall(r'\w+', articleToBeSummarized)) > 750:
+
+        return "THe length of the total article should be not greater than 750 words."
+    
+    else:
+
+        summarizedArticle = text_summarizer_pipeline(articleToBeSummarized, min_length=min_length_of_article, max_length=max_length_of_article, do_sample=False)
+
+        return summarizedArticle[0]['summary_text']
 
 
-class InputTextModel(BaseModel):
-    text: str
+
+myapp = gr.Interface(fn=greetMe, 
+                     title="Text Summarizer using Transformer",
+                     inputs=[
+                         gr.Textbox(lines=20, placeholder="enter the original text that is to be summarized", label="Text Input Field", interactive=True),
+                         gr.Slider(10, 30, value=10, step=10, label="Minimum Length of Summarized Article", info="Choose between 10 and 30"),
+                         gr.Slider(120, 150, value=120, step=10, label="Maximum Length of Summarized Article", info="Choose between 120 and 150"),
+                     ],
+                     outputs=gr.Textbox(lines=20, label="Summarized Text Output", show_copy_button=True),
+                     allow_flagging='never')
 
 
-@app.get('/')
-def welcome():
-    return {
-        'success': True,
-        'message': 'server of "text summarization using transformer" is up and running successfully.'
-    }
-
-
-@app.post('/summarize-text')
-async def summarize_text(textInput: InputTextModel):
-    return {
-        'success': True,
-        'message': 'okkk'
-    }
+if __name__=="__main__":
+    myapp.launch(show_api=False)
